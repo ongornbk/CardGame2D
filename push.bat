43fec61d11a48fc4e742fba1acbf901c6ecd230a
Irish <ongornbk@gmail.com>
2018-09-10 20:13:54 +0200

    v0.02c2

--- push.bat
+++ push.bat
@@ -10,4 +10,9 @@ echo %input% > version.ver
 git add *
 git commit -m "%input%"
 git push
+echo ################################# >> pushHistory.log
+echo DirectX11Engine >> pushHistory.log
+date /t >> pushHistory.log
+type %input% >> pushHistory.log
 pause
+:exit
